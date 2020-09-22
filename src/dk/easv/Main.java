495cc390dcfbba019dff00e20737ff8342125e30
mike lykkegaard winum <skupi1234@gmail.com>
2020-09-22 10:49:21 +0200

    implemented runnable

--- src/dk/easv/Main.java
+++ src/dk/easv/Main.java
@@ -1,8 +1,20 @@
 package dk.easv;
 
-public class Main {
+public class Main implements runnable{
+
+    private boolean running = false;
+
+    public void run(){
+        while(running){
+            running = false; // Stops infinite loop, incase you try to run it at this state
+        }
+    }
+
+    public void start(){
+        running = true; // Starts the app
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World 123");
+        new Main().start();
     }
 }
